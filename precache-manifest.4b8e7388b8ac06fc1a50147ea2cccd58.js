b9b66b715dfae775fd88062337ab07207163218f
Pablo Contreras <pabloskylosky@gmail.com>
2020-09-09 13:24:17 -0300

    Updates

---
+++ precache-manifest.4b8e7388b8ac06fc1a50147ea2cccd58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c350f1813210a13a6f265ad58a7d45c",
+    "url": "/monsters-ralodex/index.html"
+  },
+  {
+    "revision": "898f8df7b7fbcc48baf7",
+    "url": "/monsters-ralodex/static/css/main.78d6e3a8.chunk.css"
+  },
+  {
+    "revision": "169717fa014767b5a456",
+    "url": "/monsters-ralodex/static/js/2.43b9666f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-ralodex/static/js/2.43b9666f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "898f8df7b7fbcc48baf7",
+    "url": "/monsters-ralodex/static/js/main.4ed3c4f4.chunk.js"
+  },
+  {
+    "revision": "0abfcf5a83851c479ef4",
+    "url": "/monsters-ralodex/static/js/runtime-main.e41664bf.js"
+  }
+]);
